c00ab73aaf4a28bb0c5f9ed8d994c88e5aaf4089
08jhs05 <08jhs05@gmail.com>
2021-05-02 20:08:03 +0000

    add database table schemas

--- db/schema/01_menu_items.sql
+++ db/schema/01_menu_items.sql
@@ -1,7 +1,5 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
 );
