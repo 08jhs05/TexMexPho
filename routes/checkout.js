3c6895a18b144de2b62dc8d43afd9b9622d4b67c
Joacquim Mukadi <joacquimm@gmail.com>
2021-05-02 18:07:43 +0000

    router setup-JM

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,19 +1,16 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require("express");
 const router = express.Router();
-
+//require the tempDB from server.js
+const tempDB = require("../server");
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
       .then((data) => {
         const users = data.rows;
-        res.json({ users });
+        // res.json({ users });
+        const templateVariables = { temporaryData: tempDB };
+        res.render("checkout", templateVariables);
+        console.log("CHECK OUT PAGE");
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
--- routes/menu.js
+++ routes/menu.js
@@ -1,10 +1,3 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require("express");
 const router = express.Router();
 
@@ -13,7 +6,9 @@ module.exports = (db) => {
     db.query(`SELECT * FROM users;`)
       .then((data) => {
         const users = data.rows;
-        res.json({ users });
+        // res.json({ users });
+        res.render("menu");
+        console.log("MENU PAGE");
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
