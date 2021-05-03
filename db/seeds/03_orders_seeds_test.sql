420016b953e7f0b0283e4cb770937b2a0be0f5a1
08jhs05 <08jhs05@gmail.com>
2021-05-03 20:04:35 +0000

    add dummy data seeds for testing

---
+++ db/seeds/03_orders_seeds_test.sql
@@ -0,0 +1,2 @@
+INSERT INTO orders (phone, subtotal, tax, final_price) VALUES ('999 999 9999', '3000', '500', '3500');
+INSERT INTO orders (phone, subtotal, tax, final_price) VALUES ('999 999 1999', '1200', '100', '1300');
---
+++ db/seeds/04_orders_item_seeds_test.sql
@@ -0,0 +1,3 @@
+INSERT INTO orders (quantity, menu_id, order_id) VALUES (3, 2, 1); -- menu_id 2: nacho chips, order_id 1: customer #999 999 9999
+INSERT INTO orders (quantity, menu_id, order_id) VALUES (1, 13, 1); -- menu_id 13:bun cha, order_id 1: customer #999 999 9999
+INSERT INTO orders (quantity, menu_id, order_id) VALUES (4, 7, 2); -- menu_id 7: Empanada, order_id 2: customer #999 999 1999
