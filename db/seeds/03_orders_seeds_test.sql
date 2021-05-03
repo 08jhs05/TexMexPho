2507b0fecfb205c58f78907d210cd4c33217a4f9
Imtiaz Miah <imtiazmiah@gmail.com>
2021-05-03 22:41:48 +0000

    Update test cb

--- db/seeds/03_orders_seeds_test.sql
+++ db/seeds/03_orders_seeds_test.sql
@@ -1,2 +1,2 @@
-INSERT INTO orders (phone, subtotal, tax, final_price) VALUES ('999 999 9999', '3000', '500', '3500');
-INSERT INTO orders (phone, subtotal, tax, final_price) VALUES ('999 999 1999', '1200', '100', '1300');
+INSERT INTO orders (phone, subtotal, tax, final_price, time_placed) VALUES ('999 999 9999', '3000', '500', '3500','2021-05-03 18:01:02');
+INSERT INTO orders (phone, subtotal, tax, final_price, time_placed) VALUES ('999 999 1999', '1200', '100', '1300','2021-05-03 18:03:02');
